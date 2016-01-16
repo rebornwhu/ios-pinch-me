b4bb76e1b03a8dc8654eacd5e37e58b1079a9186
rebornwhu <rebornwhu@gmail.com>
2016-01-15 22:44:55 -0800

    Implement pinch and rotate

--- ios-pinch-me/ViewController.swift
+++ ios-pinch-me/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    private var imageView: UIImageView!
+    private var scale: CGFloat = 1
+    private var previousScale: CGFloat = 1
+    private var rotation: CGFloat = 0
+    private var previousRotation: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let image = UIImage(named: "yosemite-meadows")
+        imageView = UIImageView(image: image)
+        imageView.userInteractionEnabled = true
+        imageView.center = view.center
+        view.addSubview(imageView)
+        
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: "doPinch:")
+        pinchGesture.delegate = self
+        imageView.addGestureRecognizer(pinchGesture)
+        
+        let rotationGesture = UIRotationGestureRecognizer(target: self, action: "doRotate:")
+        rotationGesture.delegate = self
+        imageView.addGestureRecognizer(rotationGesture)
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailByGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func transformImageView() {
+        var t = CGAffineTransformMakeScale(scale * previousScale, scale * previousScale)
+        t = CGAffineTransformRotate(t, rotation + previousRotation)
+        imageView.transform = t
     }
 
-
+    func doPinch(gesture: UIPinchGestureRecognizer) {
+        scale = gesture.scale
+        transformImageView()
+        if gesture.state == .Ended {
+            previousScale = scale * previousScale
+            scale = 1
+        }
+    }
+    
+    func doRotate(gesture: UIRotationGestureRecognizer) {
+        rotation = gesture.rotation
+        transformImageView()
+        if gesture.state == .Ended {
+            previousRotation = rotation + previousRotation
+            rotation = 0
+        }
+    }
 }
 
